0bc78fdc7f9cfd0029a5ac423215b2225694012b
rukawata <rukawata@gmail.com>
2020-09-04 13:20:01 -0700

    s

--- deploy/terraform/terraform-units/modules/sap_system/hdb_node/variables_local.tf
+++ deploy/terraform/terraform-units/modules/sap_system/hdb_node/variables_local.tf
@@ -158,13 +158,17 @@ locals {
 
   dbnodes = flatten([
     [for idx, dbnode in try(local.hdb.dbnodes, [{}]) : {
-      "name" = try("${dbnode.name}-0", format("%sd%s%02dl%d%s", lower(local.sap_sid), lower(local.hdb_sid), idx, 0, substr(var.random-id.hex, 0, 3))),
-      "role" = try(dbnode.role, "worker")
+      "name"         = try("${dbnode.name}-0", format("%sd%s%02dl%d%s", lower(local.sap_sid), lower(local.hdb_sid), idx, 0, substr(var.random-id.hex, 0, 3))),
+      "role"         = try(dbnode.role, "worker"),
+      "admin_nic_ip" = lookup(dbnode, "admin_nic_ips", [false, false])[0],
+      "db_nic_ip"    = lookup(dbnode, "db_nic_ips", [false, false])[0]
       }
     ],
     [for idx, dbnode in try(local.hdb.dbnodes, [{}]) : {
-      "name" = try("${dbnode.name}-1", format("%sd%s%02dl%d%s", lower(local.sap_sid), lower(local.hdb_sid), idx, 1, substr(var.random-id.hex, 0, 3))),
-      "role" = try(dbnode.role, "worker")
+      "name"         = try("${dbnode.name}-1", format("%sd%s%02dl%d%s", lower(local.sap_sid), lower(local.hdb_sid), idx, 1, substr(var.random-id.hex, 0, 3))),
+      "role"         = try(dbnode.role, "worker"),
+      "admin_nic_ip" = lookup(dbnode, "admin_nic_ips", [false, false])[1],
+      "db_nic_ip"    = lookup(dbnode, "db_nic_ips", [false, false])[1]
       }
       if local.hdb_ha
     ]
@@ -213,8 +217,8 @@ locals {
     for dbnode in local.hana_database.dbnodes : {
       platform       = local.hana_database.platform,
       name           = dbnode.name
-      admin_nic_ip   = try(lookup(dbnode, "admin_nic_ips", [false, false])[idx], false),
-      db_nic_ip      = try(lookup(dbnode, "db_nic_ips", [false, false])[idx], false),
+      admin_nic_ip   = dbnode.admin_nic_ip
+      db_nic_ip      = dbnode.db_nic_ip
       size           = local.hana_database.size,
       os             = local.hana_database.os,
       authentication = local.hana_database.authentication
